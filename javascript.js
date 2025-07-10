93937f8ccf2f962ef6d4ad541ea76c3cfc6598e6
Mobolaji <captainkell0gs01@gmail.com>
2025-07-10 14:59:54 +0100

    Created two new variables to track players score, created  the playRound function

--- javascript.js
+++ javascript.js
@@ -15,3 +15,19 @@ function getHumanChoice() {
             return humanChoice;
         }
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.toLowerCase();
+        if (humanChoice === "rock" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "rock") {
+            return "You lose!";
+            computerScore++;
+        } else if (humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scisors" && computerChoice === "paper" || humanChoice === "rock" && computerChoice === "scissors") {
+            return "You Win!";
+            humanScore++;
+        } else {
+            return "Tie!";
+        }
+}
